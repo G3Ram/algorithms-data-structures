43664c94f42f90832fe39e11456cf6882f979e31
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2023-04-25 21:37:35 -0500

    feat: two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let hi = breaks.length;
+    let lo = 0;
 
+    while (lo < hi) {
+        let m = lo + (Math.floor((hi-lo)/2));
+        let v = breaks[m];
+        let pv = breaks[m-1];
+        if (v && !pv) {
+            return m;
+        } else if (v && pv) {
+            hi = m;
+        } else {
+            lo = m+1;
+        }
+    }
+    return -1;
 }
