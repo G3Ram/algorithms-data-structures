8dd324099ba0f96e5005eaab811be0f2ec02df94
Gayathri Ramakrishnan <gayathri.t.ramakrishnan@gmail.com>
2023-04-27 21:19:00 -0500

    feat: binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    if (haystack.length === 0) return false;
+    let hi = haystack.length;
+    let lo = 0;
+    let m;
+
+    while (lo < hi) {
+        m = lo + Math.floor((hi-lo)/2)
+        if (haystack[m] ===  needle) return true;
+        if (haystack[m] > needle) {
+            hi = m;
+        } else if (haystack[m] < needle) {
+            lo = m+1;
+        }
+    }
+
+    return false;
 
 }
